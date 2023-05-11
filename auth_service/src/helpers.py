057d1b38ed9251d591db4b0fb8d1cbe695d7b9a7
EzequielK-source <programacionezequielk@gmail.com>
2023-05-11 17:52:45 -0300

    Create validate_user_fields helpers

--- auth_service/src/helpers.py
+++ auth_service/src/helpers.py
@@ -2,6 +2,27 @@ from . import Session
 from .models import User
 
 
+def validate_user_fields(user: dict):
+    '''
+       Validates that the JSON object representing a user has all the required fields and that
+       the fields have the correct data types.
+
+        Args: 
+        user (dict): an json object
+            - name (str): user name
+                * len >= 4
+            - password (str): user password
+                * len > 9
+            - email (str): user email
+                * contains '@'
+
+        Raises: ValueError 
+    '''
+
+    if (len(user['name']) < 4):
+        raise ValueError()
+
+
 def is_registered_email(email):
     '''
       Return if exist an user registered with email
