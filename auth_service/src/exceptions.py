a4cc6723c852e2146833bf969557baa093a2975f
EzequielK-source <programacionezequielk@gmail.com>
2023-05-11 02:27:07 -0300

    Added validation to register_service - Now register_service raise exception if email are already registered

---
+++ auth_service/src/exceptions.py
@@ -0,0 +1,2 @@
+class AlreadyRegisteredEmail(Exception):
+    pass
--- auth_service/src/services/register.py
+++ auth_service/src/services/register.py
@@ -1,12 +1,19 @@
+from src.exceptions import AlreadyRegisteredEmail
 from src import Session
 from src.models import User
 import datetime
+
+from src.helpers import is_registered_email
 
 
 def register_service(user_data: dict):
     '''
         Persist an user to database
     '''
+
+    if (is_registered_email(user_data['email'])):
+        raise AlreadyRegisteredEmail()
+
     user = User(name=user_data['name'],
                 email=user_data['email'],
                 password=user_data['password'],
