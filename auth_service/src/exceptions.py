defe0b5ae837d6fe3367446892a71cf8e371beca
EzequielK-source <programacionezequielk@gmail.com>
2023-05-15 06:00:21 -0300

    Added new Custom Exceptions -Added parent exception to categorize all errors related to http requests

--- auth_service/src/exceptions.py
+++ auth_service/src/exceptions.py
@@ -1,2 +1,14 @@
 class AlreadyRegisteredEmail(Exception):
     pass
+
+
+class RequestExceptions(Exception):
+    pass
+
+
+class MissingParameter(RequestExceptions):
+    message = 'Bad request, Missing at least one parameter'
+
+
+class InvalidParameter(RequestExceptions):
+    message = 'Bad request, At least one parameter is invalid'
