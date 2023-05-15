fd96dad1cd65e8621960aac372fa4b69d207a4b3
EzequielK-source <programacionezequielk@gmail.com>
2023-05-15 05:45:30 -0300

    Remove validate_user_fields - Replace validate_user_fields for the UserFieldsValidator class - Create validate_name and validate_fields method - Add get_invalid_paramters

--- auth_service/src/helpers.py
+++ auth_service/src/helpers.py
@@ -2,26 +2,47 @@ from . import Session
 from .models import User
 
 
-def validate_user_fields(user: dict):
-    '''
-       Validates that the JSON object representing a user has all the required fields and that
-       the fields have the correct data types.
+class UserFieldsValidator:
+    def __init__(self, user_fields: dict):
+        self.user_fields = user_fields
+        self.invalid_parameters = []
 
-        Args: 
-        user (dict): an json object
-            - name (str): user name
-                * len >= 4
-            - password (str): user password
-                * len > 9
-            - email (str): user email
-                * contains '@'
+    def validate_fields(self):
+        '''
+        Validates that the JSON object representing a user has all the required fields and that
+        the fields have the correct data types.
 
-        Raises: ValueError 
-    '''
+            Args: 
+            user (dict): an json object
+                - name (str): user name
+                    * len >= 4
+                - password (str): user password
+                    * len > 9
+                - email (str): user email
+                    * contains '@'
 
-    if (len(user['name']) < 4):
-        raise ValueError()
+            Raises: [MissingParameter, InvalidParameter] 
+        '''
+        self.validate_name()
 
+    def validate_name(self):
+        '''
+            Validates if name is not None and have len of 4
+        '''
+        if (len(self.user_fields['name']) < 4):
+            self.invalid_parameters.append(
+                InvalidParameterInfo(
+                    name='name',
+                    value=self.user_fields['name'],
+                    reason='The field must have a minimum length of 4')
+            )
+            raise InvalidParameter()
+
+    def get_invalid_parameters(self):
+        '''
+            Returns all invalid parameters
+        '''
+        return self.invalid_parameters
 
 def is_registered_email(email):
     '''
