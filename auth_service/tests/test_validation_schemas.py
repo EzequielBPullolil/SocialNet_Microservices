1fce80cdaf00fac6ec270ed1b8abec65c18b25c4
EzequielK-source <programacionezequielk@gmail.com>
2023-05-16 22:53:55 -0300

    Added testcase for verify invalid password - Create testcase to verify that if we pass a password without number or symbol raise InvalidEschema and the error message is the expected

--- auth_service/tests/test_validation_schemas.py
+++ auth_service/tests/test_validation_schemas.py
@@ -33,6 +33,25 @@ class TestValidateUserSchema:
         name_message = e_info.value.invalid_params['name']
         assert 'The name field must have at least 4 characters' in name_message['message']
 
+    def test_invalids_password_raises_error(self):
+        '''
+            Verify if parse and schema with wrong password raise exception
+            and the error message is the expected for an short name
+
+            first parse an invalid password: 8 length with no symbol or number
+            seccond parse an short password: symbol and number but length < 8
+        '''
+        schema_with_invalid_password = {
+            'name': 'Alea',
+            'password': 'abdfghia',  # missig symbol or number
+            'email': 'anemail@test.com'
+        }
+        with pytest.raises(InvalidEschema) as e_info:
+            validate_user_schema(schema_with_invalid_password)
+
+        password_message = e_info.value.invalid_params['password']
+        assert 'The password field must have at least 1 symbol or at least 1 number' in password_message[
+            'message']
     def test_invalid_email_raise_error(self):
         '''
             Verify if parse an schema with invalid email raise InvalidEschema
