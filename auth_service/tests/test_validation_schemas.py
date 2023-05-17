8b3c90e3f748a31f5732e0f75155e1c0808d9076
EzequielK-source <programacionezequielk@gmail.com>
2023-05-16 22:50:15 -0300

    Added testcase for invalid email field - Create testcase for verify if the error message of InvalidSchema is the expected

--- auth_service/tests/test_validation_schemas.py
+++ auth_service/tests/test_validation_schemas.py
@@ -32,3 +32,19 @@ class TestValidateUserSchema:
 
         name_message = e_info.value.invalid_params['name']
         assert 'The name field must have at least 4 characters' in name_message['message']
+
+    def test_invalid_email_raise_error(self):
+        '''
+            Verify if parse an schema with invalid email raise InvalidEschema
+            and if the message is the expected for invalid email
+        '''
+        schema_with_invalid_email = {
+            'name': 'Aled',  # >= 4 length
+            'password': 'abdfghi8#',  # >= 8 length
+            'email': 'anemail.com'
+        }
+
+        with pytest.raises(InvalidEschema) as e_info:
+            validate_user_schema(schema_with_invalid_email)
+
+        assert e_info.value.invalid_params['email']['message'] == 'Invalid email format'
