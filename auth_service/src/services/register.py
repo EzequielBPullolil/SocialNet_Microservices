2a709b3452bcd2e929fe3eecf4638170da3c2194
EzequielK-source <programacionezequielk@gmail.com>
2023-05-18 01:31:28 -0300

    Now register_service encrypt password before persist user

--- auth_service/src/services/register.py
+++ auth_service/src/services/register.py
@@ -3,20 +3,25 @@ from src import Session
 from src.models import User
 import datetime
 
-from src.helpers import is_registered_email
+from src.helpers import is_registered_email, PasswordManager
 
 
 def register_service(user_data: dict):
     '''
-        Persist an user to database
+        Encrypt the password and persist user 
+        to database
     '''
-
+    password_manager = PasswordManager()
     if (is_registered_email(user_data['email'])):
         raise AlreadyRegisteredEmail()
 
+    encrypted_password = password_manager.encrypt_password(
+        user_data['password']
+    )
+
     user = User(name=user_data['name'],
                 email=user_data['email'],
-                password=user_data['password'],
+                password=encrypted_password,
                 created_at=datetime.datetime.now())
 
     session = Session()
