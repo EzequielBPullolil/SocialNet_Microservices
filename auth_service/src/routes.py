9774cca04db1d9ce3585642da0485c50487a3dd9
EzequielK-source <programacionezequielk@gmail.com>
2023-05-18 02:27:45 -0300

    Implement validate_user_fields_middleware in register endpoint

--- auth_service/src/routes.py
+++ auth_service/src/routes.py
@@ -1,5 +1,6 @@
 
 from src.exceptions import InvalidEschema
+from src.middlewares.validate_fields import validate_user_fields_middleware
 from src.validation_schemas import validate_user_schema
 from flask import Blueprint, request
 from .services.register import register_service
@@ -7,36 +8,18 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @auth_bp.route('/register', methods=['POST'])
+@validate_user_fields_middleware
 def register():
     '''
-        persist user if the json data is valid,
-        else response status 400
-
-        Args:
-            - name (str): user name
-                * len >= 4
-            - password (str): user password
-                * len > 9
-            - email (str): user email
-                * contains '@'
+        Persist and user before validate_fields_middleware
     '''
-    try:
-        validate_user_schema(request.get_json())
-        user_id = register_service(user_data=request.get_json())
-        return {
-            'status': 'success',
-            'message': 'User successfully registered',
-            'data': {
-                'id': user_id,
-                'name': request.get_json()['name'],
-                'email': request.get_json()['email']
-            }
-        }, 201
-    except InvalidEschema as excep_inf:
-        return {
-            'status': 'error',
-            'message': 'Invalid json schema',
-            'invalid_params': excep_inf.invalid_params,
-            'missing_params': excep_inf.missing_params
-
-        }, 400
+    user_id = register_service(user_data=request.get_json())
+    return {
+        'status': 'success',
+        'message': 'User successfully registered',
+        'data': {
+            'id': user_id,
+            'name': request.get_json()['name'],
+            'email': request.get_json()['email']
+        }
+    }, 201
