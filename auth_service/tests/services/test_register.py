08f56c15d8d208ff48599d1983ecf5ca44efe58d
EzequielK-source <programacionezequielk@gmail.com>
2023-05-18 01:30:18 -0300

    Added testcase to register services - Added testcase to verify if register persist an user with encrypted password

--- auth_service/tests/services/test_register.py
+++ auth_service/tests/services/test_register.py
@@ -66,3 +66,26 @@ class TestRegisterService(TestCase):
                     'email': self.registeredEmail
                 }
             )
+
+    def test_successful_register_persist_user_with_encrypted_password(self):
+        userData = {
+            'name': 'ezequiel',
+            'password': 'ezequiel_password',
+            'email': 'encryptedPasswordUser@email.com'
+        }
+        register_service(user_data=userData)
+
+        persisted_user = self.session.query(
+            User).filter_by(email=userData['email']).first()
+        self.assertIsNotNone(persisted_user)
+
+        self.assertNotEqual(
+            userData['password'], persisted_user.password
+        )
+
+        self.assertTrue(
+            self.password_manager.check_password(
+                userData['password'],
+                persisted_user.password
+            )
+        )
