d23dadb85b57c45c3b1d6ffd270750d5d24ebaca
EzequielK-source <programacionezequielk@gmail.com>
2023-05-18 00:22:19 -0300

    Added testcase for register endpoint - Added testcase for verify if missing any of the required params return an status code 400 and error json response

--- auth_service/tests/test_routes.py
+++ auth_service/tests/test_routes.py
@@ -167,48 +167,22 @@ class TestAuthRoutes:
         assert 'Bad request, Missing at least one parameter' in json_response['message']
         assert ['password'] == json_response['missing_parameters']
 
-    def test_post_request_auth_register_endpoint_with_invalid_or_missing_email_param_response_http_status_400(self, client):
+    def test_missing_any_required_param_response_status_code_400(self, client):
         '''
-          Verify if parse a invalid or missing password response with bad request and status_code 400
-          and the expected error json
-          keys (status, message, missing_parameters, invalid_parameters)
-
-          example invalid name case:
-          {
-            'status': 'error',
-            'message': 'Bad request, At least one parameter is invalid.'
-            'missing_parameters': 
-            'invalid_parameters': {
-              'name': 'email',
-              'value': 'abcdfg@a.com',
-              'reason': 'The field must have a minimum length of 6 in email domain',
-            }
-          }
-          example missing name case:
-          {
-            'status': 'error',
-            'message': 'Bad request, Missing at least one parameter.',
-            'missing_parameters': ['email'],
-            'invalid_parameters':'',
-          }
+          Verify if missing any of the following next params (name, password, email) response with status code 400
+          and the expected response
         '''
-        # Verify invalid email error response
-        invalid_email_data = copy.copy(self.register_data)
-        invalid_email_data['email'] = 'abcdfg@a.com'
-        response = client.post('/auth/register', json=invalid_email_data)
-        assert response.status_code == 400
-        assert response.content_type == 'application/json'
-        json_response = response.get_json()
+        required_params = ['name', 'password', 'email']
 
-        assert 'error' in json_response['status']
-        assert 'The email almost need' in json_response['message']
+        for missing_param in required_params:
+            user_schema = copy.copy(self.register_data)
+            del user_schema[missing_param]
 
-        # Verify missing email error response
-        del invalid_email_data['email']
-        response = client.post('/auth/register', json=invalid_email_data)
-        assert response.status_code == 400
-        assert response.content_type == 'application/json'
-        json_response = response.get_json()
+            response = client.post('/auth/register', json=user_schema)
+            assert response.status_code == 400
+            json_response = response.get_json()
 
-        assert 'error' in json_response['status']
-        assert 'Bad request, Missing at least one parameter' in json_response['message']
+            assert json_response['status'] == 'error'
+            assert json_response['message'] == 'Invalid json schema'
+
+            assert missing_param in json_response['missing_params']
