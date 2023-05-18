ea2a24e1dd29aa0158455f91d0452d981c513a93
EzequielK-source <programacionezequielk@gmail.com>
2023-05-18 01:59:22 -0300

    Move PasswordManager to file - Remove PasswordManager from helpers.py

--- auth_service/src/helpers.py
+++ auth_service/src/helpers.py
@@ -64,29 +64,6 @@ def invalid_email_message(email) -> dict:
     return email_message
 
 
-class PasswordManager:
-    salt = bcrypt.gensalt()
-
-    def encrypt_password(self, password: str):
-        '''
-          Return an encrypted password
-        '''
-        hashed_password = bcrypt.hashpw(
-            password.encode('utf-8'), self.salt
-        )
-
-        return hashed_password.decode('utf-8')
-
-    def check_password(self, password: str, hashed_password: str) -> bool:
-        '''
-            Return true if the password is the same
-        '''
-        return bcrypt.checkpw(
-            password.encode('utf-8'),
-            hashed_password.encode('utf-8')
-        )
-
-
 def invalid_password_message(password) -> dict:
     '''
         Describes custom message for invalid password
