96a6fccfed4e06dffa7a238ed37fc6f67d1b31ec
EzequielK-source <programacionezequielk@gmail.com>
2023-05-22 01:26:48 -0300

    Now the user model generates the created_at field automatically

--- auth_service/src/models.py
+++ auth_service/src/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from src import Base, engine
 
+from datetime import datetime
 
 class User(Base):
     __tablename__ = 'users'
@@ -11,11 +12,12 @@ class User(Base):
     password = Column(String)
     created_at = Column(DateTime)
 
-    def __init__(self, name, email, password, created_at):
+    def __init__(self, name, email, password):
+        date_current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
         self.name = name
         self.email = email
         self.password = password
-        self.created_at = created_at
+        self.created_at = date_current_time
 
 
 Base.metadata.create_all(engine)
