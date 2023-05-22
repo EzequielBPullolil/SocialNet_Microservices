98fb39709b13b9b8b232fd79ce8f998e1d258d3f
EzequielK-source <programacionezequielk@gmail.com>
2023-05-21 23:53:31 -0300

    Add fixture test_user - Added fixture for testing using persisted user

--- auth_service/tests/conftest.py
+++ auth_service/tests/conftest.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 import pytest
+from src import Session
 from src.app import app
+from src.helpers.password_manager import PasswordManager
+from src.models import User
 
 
 @pytest.fixture
@@ -14,3 +18,30 @@ def flask_app():
 @pytest.fixture()
 def client(flask_app):
     return flask_app.test_client()
+
+
+@pytest.fixture()
+def test_user():
+    '''
+        Persist an user with encrypted password 
+        and return they email and password
+    '''
+    password_manager = PasswordManager()
+    email = 'logintest@test_user.com'
+    password = 'password_test'
+
+    session = Session()
+    user = User(
+        name='',
+        email=email,
+        password=password_manager.encrypt_password(password),
+        created_at=datetime.now()
+    )
+    session.add(user)
+    session.commit()
+
+    session.close()
+    return {
+        'email': email,
+        'password': password
+    }
