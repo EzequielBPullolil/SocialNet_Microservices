6a580bff59874f41b4a9a9c0185d922081170000
EzequielK-source <programacionezequielk@gmail.com>
2023-05-22 01:48:16 -0300

    Added email_belongs_to_user method to EmailManager

--- auth_service/src/helpers/email_manager.py
+++ auth_service/src/helpers/email_manager.py
@@ -1,4 +1,5 @@
 from src import Session
+from src.exceptions import InvalidParameter
 from src.models import User
 
 
@@ -20,3 +21,15 @@ class EmailManager:
         return (
             response == None
         )
+
+    def email_belongs_to_user(self, email, id):
+        '''
+            Verify if the email belongs to user with id
+        '''
+        session = Session()
+        response = session.query(
+            User
+        ).filter_by(id=id).first()
+        session.close()
+        if (response.email != email):
+            raise InvalidParameter
