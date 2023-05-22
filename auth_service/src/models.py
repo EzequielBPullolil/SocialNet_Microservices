9662aaf7a27020287251d0a27674a80aa49e6501
EzequielK-source <programacionezequielk@gmail.com>
2023-05-22 01:27:44 -0300

    Created AuthToken model

--- auth_service/src/models.py
+++ auth_service/src/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from src import Base, engine
 
 from datetime import datetime
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -18,6 +19,21 @@ class User(Base):
         self.email = email
         self.password = password
         self.created_at = date_current_time
+
+
+class AuthToken(Base):
+    __tablename__ = 'auth_token'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,
+                     ForeignKey('users.id'))
+    created_at = Column(DateTime)
+    token = Column(String)
+
+    def __init__(self, user_id, token):
+        date_current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+        self.created_at = date_current_time
+        self.user_id = user_id
+        self.token = token
 
 
 Base.metadata.create_all(engine)
