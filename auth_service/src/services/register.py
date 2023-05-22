c2955a7640ffa8a259e579faeacdd8b9a754104e
EzequielK-source <programacionezequielk@gmail.com>
2023-05-22 03:53:14 -0300

    Refact register_service to RegisterService class

--- auth_service/src/services/register.py
+++ auth_service/src/services/register.py
@@ -7,29 +7,78 @@ from src.helpers.email_manager import EmailManager
 from src.helpers.password_manager import PasswordManager
 
 
-def register_service(user_data: dict):
+class RegisterService:
     '''
-        Encrypt the password and persist user 
-        to database
+    This class encapsulates all the logic of registering a user
     '''
     password_manager = PasswordManager()
     email_manager = EmailManager()
-    if (not email_manager.is_avaible_the_email(user_data['email'])):
-        raise AlreadyRegisteredEmail()
 
-    encrypted_password = password_manager.encrypt_password(
-        user_data['password']
-    )
+    def register(self, register_data):
+        '''
+            Register user if the email is not in use and 
+            persist an user with encrypted password
 
-    user = User(name=user_data['name'],
-                email=user_data['email'],
-                password=encrypted_password)
+            Args: 
+                register_data(dict): The register data
+                    - 'email': The user email
+                    - 'name':  The user name
+                    - 'password': The user password
+            Return:
+                - user_data(dict)
+                    - id: The id of register user
+                    - name: The name of register user
+                    - email: The email of register user
 
-    session = Session()
-    session.add(user)
-    session.commit()
+            Raises:
+                - AlreadyRegisteredEmail
+        '''
+        self.__is_avaible_the_email(register_data['email'])
 
-    user_id = user.id
-    session.close()
+        self.__encrypt_password(register_data['password'])
+        user_id = self.__persist_user(register_data)
 
-    return user_id
+        return {
+            'id': user_id,
+            'name': register_data['name'],
+            'email': register_data['email']
+        }
+
+    def __is_avaible_the_email(self, email):
+        '''
+            Raises an exception if the email ins not avaible
+        '''
+        if (not self.email_manager.is_avaible_the_email(email)):
+            raise AlreadyRegisteredEmail()
+
+    def __persist_user(self, user_data):
+        '''
+        Persist user with encrypted password in database 
+        and return they user id
+
+
+        Args: 
+                register_data(dict): The register data
+                    - 'email': The user email
+                    - 'name':  The user name
+                    - 'password': The user password
+        Return:
+            user_id(int): The persisted user id
+        '''
+        user = User(name=user_data['name'],
+                    email=user_data['email'],
+                    password=self.encrypted_password)
+        session = Session()
+        session.add(user)
+        session.commit()
+        user_id = user.id
+        session.close()
+
+        return user_id
+
+    def __encrypt_password(self, password):
+        '''
+            Encrypt password and save it in encrypted_password property 
+        '''
+        self.encrypted_password = self.password_manager.encrypt_password(
+            password)
