f0c88f0d643eb9110bf82017f9130f8ddc461793
EzequielK-source <programacionezequielk@gmail.com>
2023-05-22 00:54:14 -0300

    Created TokenManager test - Added testcase for verify the correct token generation

---
+++ auth_service/tests/helpers/test_token_manager.py
@@ -0,0 +1,43 @@
+import jwt
+from src.helpers.token_manager import TokenManager
+
+
+class TestTokenManager:
+    '''
+        This test verifies that TokenManager works correctly
+
+        Using the following test cases as a guide:
+            - generate_token method generates valid token
+              * assertions: 
+                1. is token valid
+                2. is encrypted with RSA
+            - validate_token raise exception if
+              - the token is invalid
+              - the token is expired
+    '''
+    token_manager = TokenManager()
+    with open('public_key.pem', 'rb') as f:
+        public_key = f.read()
+
+    def test_generate_token_generate_valid_token(self, test_user):
+        '''
+          Verify if generate_token method generated a
+          encrypted WITH RSA token 
+        '''
+        credentials = {
+            "user_id": test_user['user_id'],
+            "email": test_user['email']
+        }
+
+        token = self.token_manager.generate_token(credentials)
+        assert token != None
+        # Verify if the token is encrypted using RSA
+        header = jwt.get_unverified_header(token)
+        assert header['alg'] == 'RS256'
+
+        # Verify the token content
+        decoded_token = jwt.decode(
+            token, self.public_key,  algorithms=['RS256'])
+
+        assert decoded_token['user_id'] == credentials['user_id']
+        assert decoded_token['email'] == credentials['email']
